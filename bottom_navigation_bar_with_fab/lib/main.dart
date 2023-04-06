112d4395eb31acea1451fa83d253243d097c9da3
Brice G <bricegoudalo@gmail.com>
2023-04-06 13:30:29 +0200

    Rm debug ModeBanner

--- bottom_navigation_bar_with_fab/lib/main.dart
+++ bottom_navigation_bar_with_fab/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Navigation bottom bar with FAB',
       home: Home(),
     );
