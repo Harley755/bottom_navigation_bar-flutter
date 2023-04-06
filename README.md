50e7cd71891276bcf595579f2839415aaa2221cf
Brice G <bricegoudalo@gmail.com>
2023-04-06 12:49:33 +0200

    update simple BNB

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # bottom_navigation_bar-flutter
+
+## Getting Started
+
+Run the following commands to start project configuration : 
+```
+git clone https://github.com/Harley755/online_bag_shop-flutter.git
+cd online_bag_shop-flutter
+flutter pub get
+```
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
--- simple_bottom_navigation_bar/lib/main.dart
+++ simple_bottom_navigation_bar/lib/main.dart
@@ -1,93 +1,18 @@
-import 'package:simple_bottom_navigation_bar/screens/home_screen.dart';
-import 'package:simple_bottom_navigation_bar/screens/notification_screen.dart';
-import 'package:simple_bottom_navigation_bar/screens/profileScreen.dart';
-import 'package:simple_bottom_navigation_bar/screens/search_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_bottom_navigation_bar/home.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int currentIndex = 0;
-  void onTap(int index) {
-    setState(() {
-      currentIndex = index;
-    });
-  }
-
-  List page = [
-    const HomeScreen(),
-    const SearchScreen(),
-    const NotificationScreen(),
-    const ProfileScreen(),
-  ];
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Bottom Navigation Bar with PageView',
-      home: Scaffold(
-        body: page[currentIndex],
-        bottomNavigationBar: buildNavigationBar(),
-      ),
-    );
-  }
-
-  Container buildNavigationBar() {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.only(
-          topRight: Radius.circular(20.0),
-          topLeft: Radius.circular(20.0),
-        ),
-      ),
-      child: BottomNavigationBar(
-        backgroundColor: Colors.transparent,
-        currentIndex: currentIndex,
-        onTap: (value) => onTap(value),
-        selectedItemColor: Colors.white,
-        selectedFontSize: 12,
-        unselectedFontSize: 12,
-        unselectedItemColor: Colors.grey.withOpacity(0.7),
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: currentIndex == 0
-                ? const Icon(Icons.home, size: 30.0)
-                : const Icon(Icons.home_outlined),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: currentIndex == 1
-                ? const Icon(Icons.search, size: 30.0)
-                : const Icon(Icons.search_outlined),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: currentIndex == 2
-                ? const Icon(Icons.notifications, size: 30.0)
-                : const Icon(Icons.notifications_outlined),
-            label: 'Notifications',
-          ),
-          BottomNavigationBarItem(
-            icon: currentIndex == 3
-                ? const Icon(Icons.person, size: 30.0)
-                : const Icon(Icons.person_outlined),
-            label: 'Profile',
-          ),
-        ],
-      ),
+    return const MaterialApp(
+      title: 'Simple Bottom Navigation Bar',
+      home: Home(),
     );
   }
 }
